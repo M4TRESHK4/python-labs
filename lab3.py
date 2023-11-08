ed7807aff0920759673c0da4f2aad03ba893a20b
M4TRESHK4 <pleskatse@gmail.com>
2023-11-08 22:40:20 +0300

    task2 done

--- lab3.py
+++ lab3.py
@@ -1,18 +1,47 @@
-with open('F1.txt', 'w') as f1:
-    while True:
-        data = input("Введите данные(пустая строка для завершения ввода)")
-        if not data:
-            break
-        f1.write(data+'\n')
+def task1():
 
-with open('F1.txt', 'r') as f1, open('F2.txt', 'w') as f2:
-    lines = f1.readlines()
-    if len(lines)>=4:
-        f2.writelines(lines[3:])
+    with open('F1.txt', 'w') as f1:
+        while True:
+            data = input("Введите данные(пустая строка для завершения ввода)")
+            if not data:
+                break
+            f1.write(data+'\n')
 
-with open('F2.txt', 'r') as f2:
-    first_line = f2.readline()
-    words = first_line.split()
-    word_count = len(words)
+    with open('F1.txt', 'r') as f1, open('F2.txt', 'w') as f2:
+        lines = f1.readlines()
+        if len(lines)>=4:
+            f2.writelines(lines[3:])
+
+    with open('F2.txt', 'r') as f2:
+        first_line = f2.readline()
+        words = first_line.split()
+        word_count = len(words)
+        
+    print(f"Количество слов в первой строке файла F2: {word_count}")
+
+def task2():
+
+    with open("testTask2.txt", 'a'):
+        pass
+
+    with open('testTask2.txt', 'r') as testTxt:
+        lines = testTxt.readlines()
+
+    import codecs #это чтоб русские символы выводились
+
+    translator_dict = { # можешь добавлять сколько угодно цифр и преводов
+        "1" : "Один",
+        "2" : "Два",
+        "3" : "Три",
+        "4" : "Четыре"
+    }
+    with codecs.open("testTask2Result.txt", 'w', 'utf-8') as f: #это чтоб русские символы выводились
+
+        for line in lines:
+
+            line_data = line.split()
+
+            f.write(translator_dict[line_data[2]] + ' ' + " ".join(line_data[1:]) + u'\n') # тут буква u для того же
+            
+def task3():
     
-print(f"Количество слов в первой строке файла F2: {word_count}")
