6c4ade7aba882727f539fe3fe2555e15b7d5a79b
M4TRESHK4 <pleskatse@gmail.com>
2023-11-20 13:19:04 +0300

    светофоры

--- lab4.py
+++ lab4.py
@@ -1,4 +1,6 @@
 import math
+import time
+
 
 class Circle:
     radius = 12
@@ -19,3 +21,27 @@ class Circle:
 
 Circle.squre()
 Circle.length()
+
+class TrafficLight:
+    def __init__(self):
+        self.__color = "red"
+
+    def running(self):
+        while True:
+            if self.__color == "red":
+                print("Red light. Stop!")
+                time.sleep(7)
+                self.__color = "yellow"
+
+            elif self.__color == "yellow":
+                print("Yellow light. Get ready!")
+                time.sleep(2)
+                self.__color = "green"
+
+            elif self.__color == "green":
+                print("Green light. Go!")
+                time.sleep(5)
+                self.__color = "red"
+
+traffic_light = TrafficLight()
+traffic_light.running()
